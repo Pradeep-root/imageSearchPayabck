40945817fd2ce57b626ffb6acbbcc90afa5c80da
Pradeep <pradeepemc3@gmail.com>
2021-04-16 09:51:11 +0530

    layout design of detail screen

---
+++ app/src/main/java/com/pradeep/payback/ui/searchimage/ImageSearchAdapter.kt
@@ -0,0 +1,26 @@
+package com.pradeep.payback.ui.searchimage
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ImageSearchAdapter() : RecyclerView.Adapter<ImageSearchAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ImageSearchAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onBindViewHolder(holder: ImageSearchAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
